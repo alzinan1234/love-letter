c5d93bf7c5b976c39a9cd919956ab79e12210195
alzinan <pappurohan1100@gmail.com>
2022-08-18 00:41:56 -0700

    first commit

---
+++ love.js
@@ -0,0 +1,8 @@
+document.getElementById('yes').addEventListener('click',function(){
+    document.getElementById('msg').innerText = "I LOVE YOU NOUSIN JAHAN 💖😥";
+
+    
+})
+document.getElementById('of-course').addEventListener('click',function(){
+    document.getElementById('msg').innerText = "I LOVE YOU 💖 . If I Get You, I will have Nothing to Ask Allah in this World😥";
+})
